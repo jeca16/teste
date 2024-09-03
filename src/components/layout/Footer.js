6952748350f27f317683fa3bc65d8aded783dd82
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2024-09-03 09:24:12 -0300

    adicionar fim da aula 16

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,18 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer (){
-    return <footer>rodape</footer>
+    return (
+        <footer>
+            <ul className={styles.social_list}>
+                <li><FaFacebook/></li>
+                <li><FaInstagram/></li>
+                <li><FaLinkedin/></li>
+            </ul>
+            <p>nosso rodape</p>
+        </footer>
+    )
+        
 }
 
 export default Footer
